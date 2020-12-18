a8bc80db793419d240646a9e6ec9ab0a4bbae44d
Kyle G <legokyle@frontier.com>
2020-12-17 19:38:06 -0800

    Updated JS with RAWG API

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,7 @@
 var key = "AIzaSyDTBCUxVbL39WQeQqdnu6uFJ5t5a2989_s";
 var search = "";
 queryURL = "https://www.googleapis.com/books/v1/volumes?q=";
+queryURLTwo = "https://rawg-video-games-database.p.rapidapi.com/games?search=" + search + "a688fc83bamsh46670efb1f6f6d4p1aabd2jsncc1d3e1bf328"
 // function to search through books
 function displayBooks() {
   search = $("#search").val().trim();
@@ -34,11 +35,11 @@ function displayBooks() {
         var imageLink = item.volumeInfo.imageLinks.thumbnail;
         console.log(imageLink);
         console.log(image2Link);
-        var imgEl$ = $("<img>").attr("src", imageLink);
+        var imgEl$ = $("<img id='bookImg'>").attr("src", imageLink);
         var title2El$ = $("<p>").html(JSON.stringify(item2.volumeInfo.title));
         var author2El$ = $("<p>").html(JSON.stringify(item2.volumeInfo.authors));
         var image2Link = item2.volumeInfo.imageLinks.thumbnail;
-        var img2El$ = $("<img>").attr("src", image2Link);
+        var img2El$ = $("<img id='bookImg'>").attr("src", image2Link);
         $(div).append(titleEl$, authorEl$, imgEl$, title2El$, author2El$, img2El$);
       }
 
@@ -47,6 +48,31 @@ function displayBooks() {
     results(response);
   });
 }
+
+function displayGames() {
+  search = $("#search").val().trim();
+  const settings = {
+	  "async": true,
+	  "crossDomain": true,
+	  "url": "https://rawg-video-games-database.p.rapidapi.com/games?search=" + search,
+	  "method": "GET",
+	  "headers": {
+		  "x-rapidapi-key": "a688fc83bamsh46670efb1f6f6d4p1aabd2jsncc1d3e1bf328",
+		  "x-rapidapi-host": "rawg-video-games-database.p.rapidapi.com"
+	  }
+  };
+
+  $.ajax(settings).then(function (response) {
+    console.log(response);
+    console.log(response.results[0].name);
+    var div = $("<div>");
+    var titleEl$ = $("<p>").html(response.results[0].name);
+    var imgEl$ = $("<img id='gameImg'>").attr("src", response.results[0].background_image);
+    $(div).append(titleEl$,imgEl$);
+    $(".results").html(div);
+  });
+}
+
 // click handler to run when add input and search button when clicked
 $("#books").on("click", function () {
   var searchDiv = $("<div>");
@@ -75,7 +101,7 @@ $("#videoGames").on("click", function () {
   $(searchDiv).append(inputField, searchButton);
   $(".results").html(searchDiv);
 
-  $("#searchBtn").on("click");
+  $("#searchBtn").on("click", displayGames);
 });
 // adds 3 divs for each list
 $("#list").on("click", function () {
